7cc1ee1ee3b6050a4dbd16ae498e1c2210e0f3a3
Johannes Norenes <joey200089@gmail.com>
2019-03-19 13:48:32 +0100

    La til Level 1

---
+++ maps/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="tileset" tilewidth="32" tileheight="32" tilecount="63" columns="7">
+ <image source="tileset.png" width="224" height="288"/>
+</tileset>
